9dd66ef0f7d7c92756d94afc406d6fbbb8bd92b4
pankajsbisht143 <pankajsbisht143@gmail.com>
2023-10-24 12:22:51 +0530

    done

---
+++ src/components/BlogCard.jsx
@@ -0,0 +1,82 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import ModeEditIcon from "@mui/icons-material/ModeEdit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { Box, IconButton } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import axios from "axios";
+
+export default function BlogCard({
+  title,
+  description,
+  image,
+  username,
+  time,
+  id,
+  isUser,
+}) {
+  const navigate = useNavigate();
+  const handleEdit = () => {
+    navigate(`/blog-details/${id}`);
+  };
+
+  const handleDelete = async () => {
+    try {
+      const { data } = await axios.delete(`/api/v1/blog/delete-blog/${id}`);
+      if (data?.success) {
+        alert("Blog Deleted");
+        window.location.reload();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+  return (
+    <Card
+      sx={{ mt: 5, boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",
+        ":hover:": {
+          boxShadow: "10px 10px 20px #ccc",
+        },
+      }}
+    >
+      
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500]}} aria-label="recipe">
+            {username}
+          </Avatar>
+        }
+        title={username}
+        subheader={time}
+      />
+      <CardMedia component="img" height="194" image={image} alt="Paella dish" />
+      <CardContent>
+        <Typography variant="h6" color="text.secondary">
+          Title : {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Description : {description}
+        </Typography>
+      </CardContent>
+      {isUser && (
+        <Box display={"flex"} justifyContent={"flex-end"}>
+          <IconButton onClick={handleEdit}>
+            <ModeEditIcon color="info" />
+          </IconButton>
+          <IconButton onClick={handleDelete}>
+            <DeleteIcon color="error" />
+          </IconButton>
+        </Box>
+      )}
+    </Card>
+  );
+}
